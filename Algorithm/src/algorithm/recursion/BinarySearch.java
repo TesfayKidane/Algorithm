51a1aef95bf779f9b4aa38e815f61e89ef87ef9c
Tesfay <Tesfay@172.17.2.34>
2017-01-17 23:06:24 -0600

    binary search iterative

--- Algorithm/src/algorithm/recursion/BinarySearch.java
+++ Algorithm/src/algorithm/recursion/BinarySearch.java
@@ -5,24 +5,64 @@
  */
 package algorithm.recursion;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  *
  * @author Tesfay
  */
 public class BinarySearch {
-    
-    public static boolean binarySearch(Integer[] data, int target, int low, int high) throws IllegalStateException{
-        
-        if(data.length == 0 || low < 0 || high < 0 || low > high) return false;
-        int mid = (low + high)/2;
-        if(target == data[mid])
-            return true; // match found
-        else if( target < data[mid]){
-            return binarySearch(data, target, low, mid-1);
-        }           
-        else{
-            
-            return binarySearch(data, target, mid+1, high);
+
+    public static boolean binarySearchRecursive(Integer[] data, int target, int low, int high) throws IllegalStateException {
+
+        if (data.length == 0 || low < 0 || high < 0 || low > high) {
+            return false;
         }
+        int mid = (int)((low + high) / 2);
+        if (target == data[mid]) {
+            return true; // match found
+        } else if (target < data[mid]) {
+            return binarySearchRecursive(data, target, low, mid - 1);
+        } else {
+
+            return binarySearchRecursive(data, target, mid + 1, high);
+        }
+    }
+
+    //assumption is the input array is sorted
+    public static boolean binarySearchIterative(Integer[] data, int target) {
+
+        int low = 0;
+        int high = data.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;  // find the midian
+            if (target == data[mid]) {
+                return true;  // match found
+            } else if (target < data[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false; // no match found.  
+    }
+
+    public static void main(String[] args) {
+         int target = 90;
+        int low = 0 ;
+        int high = 100;
+//        Integer[] ints = new Integer[high];
+//        List<Integer> list = IntStream.rangeClosed(low, high+1).boxed().collect(Collectors.toList());
+//        list.toArray(ints);
+        
+        Integer[] ints = {0,1,2,3,4,5,6,7,8,9,10};
+        long start = System.nanoTime();
+        System.out.println("binarySearchRecursive : " + binarySearchRecursive(ints, target, low, high)+ ", time : " + (System.nanoTime() - start));
+        
+        start = System.nanoTime();
+        System.out.println("binarySearchIterative : " + binarySearchIterative(ints, target)+ ", time : " + (System.nanoTime() - start));
     }
 }

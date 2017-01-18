4fe5a7e2739b7aa026de5c59984a41a4916e6ce5
Tesfay <Tesfay@172.17.2.34>
2017-01-18 01:28:23 -0600

    fibonacci with for loop

--- Algorithm/src/algorithm/fibonacci/Fibonacci.java
+++ Algorithm/src/algorithm/fibonacci/Fibonacci.java
@@ -1,6 +1,6 @@
 /*
 * Write function to compute Nth fibonacciBad number:
-*/
+ */
 package algorithm.fibonacci;
 
 /**
@@ -8,30 +8,48 @@ package algorithm.fibonacci;
  * @author Tesfay
  */
 public class Fibonacci {
-    // computes is O(pow(2,n)) time
-    public static int fibonacciBad(int n){
-        return n <= 1 ? n : fibonacciBad(n-1) + fibonacciBad(n-2);
+
+    // computes in O(pow(2,n)) time
+    public static int fibonacciBad(int n) {
+        return n <= 1 ? n : fibonacciBad(n - 1) + fibonacciBad(n - 2);
     }
-    
-    // computes is O(n) time
-    public static long[] fibonacciGood(int n)
-    {
-        if(n <= 1 ){
-            long[] answer = {n,0};
+
+    // computes in O(n) time
+    public static long[] fibonacciGood(int n) {
+        if (n <= 1) {
+            long[] answer = {n, 0};
             return answer;
-        }
-        else{
-            long[] temp = fibonacciGood(n-1); // returns fn-1, fn-2
+        } else {
+            long[] temp = fibonacciGood(n - 1); // returns fn-1, fn-2
             long[] answer = {temp[0] + temp[1], temp[0]}; // we want fn,fn-1
             return answer;
         }
     }
-    
-    public static void main(String[] args){
-        int n = 100;
+    // computes in O(n) time
+    public static int findFibonacci(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        int fibo1 = 0;
+        int fibo2 = 1;
+
+        int fibo = fibo2;
+        for (int i = 1; i < n; i++) {
+            fibo = fibo1 + fibo2;
+            fibo1 = fibo2;
+            fibo2 = fibo;
+        }
+
+        return fibo;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
         long start = System.nanoTime();
-        System.out.println("fibBad : " + fibonacciBad(n) + " time : " + (System.nanoTime() - start));
-        start = System.nanoTime();       
-        System.out.println("fibGood : " +  fibonacciGood(n)[0] + " time : " + (System.nanoTime() - start));
+        System.out.println("fibonacciBad : " + fibonacciBad(n) + " time : " + (System.nanoTime() - start));
+        start = System.nanoTime();
+        System.out.println("fibonacciGood : " + fibonacciGood(n)[0] + " time : " + (System.nanoTime() - start));
+        start = System.nanoTime();
+        System.out.println("findFibonacci : " + findFibonacci(n) + " time : " + (System.nanoTime() - start));
     }
 }
